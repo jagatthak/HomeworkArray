9e98f7f80f19fa81bd3ad2d58b126629dd4a5b17
jagatthak <jagatthakkar25@gmail.com>
2022-05-13 21:25:52 +0100

    Homeworkarray

---
+++ src/main/java/Max_Min_Array.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Max_Min_Array {
+       public static void main(String[] args) {
+        int [] a ={25,38,15,28,55,44};
+        //sort array to find max and min value
+        Arrays.sort(a);
+        int min =a[0];
+           System.out.println("Minimum value of array a: "+min);
+       int max=a[a.length-1];
+           System.out.println("Maximum value of array a: "+max);
+       }
+}
